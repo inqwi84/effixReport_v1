0a2b4e2bfe51df6dd7011cf4d871c6a5cb19d5b4
sp_inq <sp_inq@mail.ru>
2012-12-05 02:09:13 +0400

    добавлено дерево!

--- EffixReportSystem/EffixReportSystem/Views/Publication/ViewModels/ViewPublicationViewModel.cs
+++ EffixReportSystem/EffixReportSystem/Views/Publication/ViewModels/ViewPublicationViewModel.cs
@@ -14,11 +14,42 @@ namespace EffixReportSystem.Views.Publication.ViewModels
 
         private ObservableCollection<EF_Project> _allDepartments;
         private ObservableCollection<EF_Publication> _allPublications;
+        public ObservableCollection<EF_Project> AllDepartments
+        {
+            get
+            {
+                return this._allDepartments;
+            }
+            set
+            {
+                if (AllDepartments == value)
+                    return;
+
+                _allDepartments = value;
+                this.OnPropertyChanged("AllDepartments");
+            }
+        }
         private void GetAllDepartments()
         {
             using (var model = new EntitiesModel())
             {
                 _allDepartments = new ObservableCollection<EF_Project>(model.EF_Projects);
+                foreach (var dp in _allDepartments)
+                {
+                    dp.Children=new ObservableCollection<EF_SMI_Type>(model.EF_SMI_Types);
+                    foreach (var smi in dp.Children)
+                    {
+                        smi.Children=new ObservableCollection<Year>(){new Year(){Name = "2012"}};
+                        foreach (var year in smi.Children)
+                        {
+                            year.Children=new ObservableCollection<Month>(){new Month(){Name = "01"}};
+                            foreach (var months in year.Children)
+                            {
+                                months.Children=new ObservableCollection<Day>(){new Day(){Name = "01"}};
+                            }
+                        }
+                    }
+                }
             }
         }
         private void GetAllPublications()
@@ -26,50 +57,32 @@ namespace EffixReportSystem.Views.Publication.ViewModels
             using (var model = new EntitiesModel())
             {
                 _allPublications = new ObservableCollection<EF_Publication>(model.EF_Publications);
-                var tfirst = _allPublications.GroupBy(item => item.Project_id);
-                var tsecond = _allPublications.GroupBy(item => item.Project_id);
                 foreach (var dept in _allDepartments)
                 {
                     foreach (var years in dept.Children)
                     {
                         foreach (var year in years.Children)
                         {
-                            foreach (var months in year.Children)
+                            var year1 = year;
+                            foreach (var months in year1.Children)
                             {
-                                foreach (var day in months.Children)
+                                var months1 = months;
+                                foreach (var day in months1.Children)
                                 {
                                     var day1 = day;
-                                    day.Children=new ObservableCollection<EF_Publication>(model.EF_Publications.Where(item=>item.P_day==day1.Name));
+                                    day.Children=new ObservableCollection<EF_Publication>(model.EF_Publications.Where(item=>item.P_day==day1.Name&&item.P_month==months1.Name&&item.P_year==year1.Name));
                                 }
                             }
                         }
                     }
                 }
-                //foreach (var pb in tfirst)
-                //{
-                //    foreach (var department in _allDepartments)
-                //    {
-                //        if(pb.Key==department.Project_id)
-                //        {
-                //            var years = pb.GroupBy(item => item.P_year);
-                //            foreach (var year in years)
-                //            {
-                //                if(year.Key)
-                //            }
-                //            foreach (var efPublication in pb)
-                //            {
-                //                efPublication.
-                //            }
-                //        }
-                //    }
-                    
-                //}
-
             }
         }
         public ViewPublicationViewModel()
         {
+            GetAllDepartments();
             GetAllPublications();
+            AllDepartments = _allDepartments;
         }
     }
 }
