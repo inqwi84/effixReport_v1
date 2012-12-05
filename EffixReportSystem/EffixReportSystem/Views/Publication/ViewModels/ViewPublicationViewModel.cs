eda2621e2b9f91ed719637c6dff4b4624a19b4c7
sp_inq <sp_inq@mail.ru>
2012-12-05 09:24:45 +0400

    05-12-2012

--- EffixReportSystem/EffixReportSystem/Views/Publication/ViewModels/ViewPublicationViewModel.cs
+++ EffixReportSystem/EffixReportSystem/Views/Publication/ViewModels/ViewPublicationViewModel.cs
@@ -36,9 +36,11 @@ namespace EffixReportSystem.Views.Publication.ViewModels
                 _allDepartments = new ObservableCollection<EF_Project>(model.EF_Projects);
                 foreach (var dp in _allDepartments)
                 {
+                    dp.Name = dp.Project_name;
                     dp.Children=new ObservableCollection<EF_SMI_Type>(model.EF_SMI_Types);
                     foreach (var smi in dp.Children)
                     {
+                        smi.Name = smi.Smi_type_name;
                         smi.Children=new ObservableCollection<Year>(){new Year(){Name = "2012"}};
                         foreach (var year in smi.Children)
                         {
@@ -59,9 +61,11 @@ namespace EffixReportSystem.Views.Publication.ViewModels
                 _allPublications = new ObservableCollection<EF_Publication>(model.EF_Publications);
                 foreach (var dept in _allDepartments)
                 {
-                    foreach (var years in dept.Children)
+                    var dept1 = dept;
+                    foreach (var smi in dept1.Children)
                     {
-                        foreach (var year in years.Children)
+                        var smi1 = smi;
+                        foreach (var year in smi1.Children)
                         {
                             var year1 = year;
                             foreach (var months in year1.Children)
@@ -70,7 +74,13 @@ namespace EffixReportSystem.Views.Publication.ViewModels
                                 foreach (var day in months1.Children)
                                 {
                                     var day1 = day;
-                                    day.Children=new ObservableCollection<EF_Publication>(model.EF_Publications.Where(item=>item.P_day==day1.Name&&item.P_month==months1.Name&&item.P_year==year1.Name));
+                                    day.Children = new ObservableCollection<EF_Publication>(_allPublications.Where(
+                                        item=>item.P_day==day1.Name&&
+                                        item.P_month==months1.Name&&
+                                        item.P_year==year1.Name&&
+                                        item.Smi_id == smi1.Smi_type_id&&
+                                        item.Project_id == dept1.Project_id
+                                        ));
                                 }
                             }
                         }
